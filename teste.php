3668a915f67df3321acdd44b680b697eef38df79
LuizBlume <luiz.fernando.251107@gmail.com>
2025-10-15 13:50:55 -0300

    Add initial PHP files for SWeb1 project including banco.php, index.html, and teste.php

---
+++ teste.php
@@ -0,0 +1,13 @@
+<?php
+$ipv4 = $_SERVER["REMOTE_ADDR"];
+echo "<body style='background-color: #ADD8E6;'>";
+echo "<center>";
+echo "<img src='https://abre.ai/banneracme'>";
+echo "<h1>Página de teste PHP no Servidor SWeb1</h1>";
+echo "<h1>Luiz Fernando e Joao Portela - 7401-6511</h1>";
+echo "<h1>Servidor SWeb1</h1>";
+echo "<h1>Endereço IP do Servidor: $ipv4</h1>";
+echo "</center>";
+phpinfo();
+echo "</body>";
+?>
